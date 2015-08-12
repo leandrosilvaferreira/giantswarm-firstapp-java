e82e5c693f5fb29e94a45d9d839a6262f2bd91c5
Puja <puja@giantswarm.io>
2015-08-12 17:01:34 +0200

    added exit(0)
    but it still goes to failed state.

--- src/main/java/currentweather/App.java
+++ src/main/java/currentweather/App.java
@@ -26,6 +26,7 @@ public class App {
             public void run() {
                 System.out.println("Stopping gracefully");
                 Thread.currentThread().interrupt();
+                System.exit(0);
             }
         });
 
